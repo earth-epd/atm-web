f81ddfeedf6c1af382d0e20b412124edb7769b06
earth-epd <pongsakorn.de@ku.th>
2021-01-31 21:36:30 +0700

    atm delete

--- src/main/java/th/ac/ku/atm/controller/BankAccountController.java
+++ src/main/java/th/ac/ku/atm/controller/BankAccountController.java
@@ -1,5 +1,6 @@
 package th.ac.ku.atm.controller;
 
+import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -47,6 +48,7 @@ public class BankAccountController {
         model.addAttribute("bankaccounts",accountService.getBankAccounts());
         return "redirect:bankaccount";
     }
+    
 }
 
 
